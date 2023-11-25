c2ae60115c1756b29a1aca0905426dac45c15082
Justin-Thomas-34 <justinthomasus34@gmail.com>
2023-11-25 14:32:54 -0500

    program_1

---
+++ Program_01.py
@@ -0,0 +1,21 @@
+# Program 1 : Program to Add Two Numbers in Python
+
+
+#  There are two methods to solve this problem
+# 1) Predefined Variables
+# 2) By taking User input
+
+
+# Solution 1 : Predefined Variables
+
+num1 = 20
+num2 = 10
+sum1 = num1 + num2
+print("The sum of two numbers is",sum1)
+
+
+# Solution 2 : By taking user input
+num3 = float(input("Enter a number"))
+num4 = float(input("Enter second number"))
+sum2 = num3 + num4 
+print("The sum of two numbers is",sum2)
