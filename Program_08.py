6866bd3d3303370ebf3eddfddeb5ed7c2683f19c
Justin-Thomas-34 <justinthomasus34@gmail.com>
2023-11-27 11:34:40 -0500

    Program_08

---
+++ Program_08.py
@@ -0,0 +1,9 @@
+# Python Program to Convert Kilometers to Miles
+
+# Formula: 1 km = 0.621371 miles
+
+# Solution:
+
+km = float(input("Enter your value in km: "))
+miles = km * 0.6213711
+print(km, "km  is ",miles,"miles")
