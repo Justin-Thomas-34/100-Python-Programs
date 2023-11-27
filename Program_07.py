bee1acbf40e6dc99491fc8cc2fa405e63bf929db
Justin-Thomas-34 <justinthomasus34@gmail.com>
2023-11-27 11:34:30 -0500

    Program_07

---
+++ Program_07.py
@@ -0,0 +1,7 @@
+# Program 07 : Python Program to Generate Random Number 
+
+# Solution: Using Random module
+
+import random           #importing random module
+num = random.randint(0,10) # random number between 0 and 10 including 0 and 10
+print(num)
