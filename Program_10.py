75c16a68b010ffa1c8772206d1560f878d00f436
Justin-Thomas-34 <justinthomasus34@gmail.com>
2023-11-28 10:14:56 -0500

    Program_10

---
+++ Program_10.py
@@ -0,0 +1,14 @@
+# Program 10: Python Program to Check if Number is Positive, Negative or 0
+
+# If number is greater than zero it is positive number. If number is less than zero it is negative number.
+
+# Solution: using Conditional Statements
+
+num = int(input("Enter a number: "))
+if num > 0:
+    print("Entered number is a positive number")
+elif num < 0:
+    print("Entered number is a negative number")
+else:
+    print("Entered number is zero")
+
