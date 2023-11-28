5652ac181a4f862e242fd0fb59f2db97b5ece938
Justin-Thomas-34 <justinthomasus34@gmail.com>
2023-11-28 10:15:07 -0500

    Program_11

---
+++ Program_11.py
@@ -0,0 +1,11 @@
+# Program 11: Python Program to Check if a number is Odd or Even
+
+# if a number is divisible by 2 it is even number. if a number is not divisible by 2 it is even number
+
+#  Solution:
+
+num = float(input("Enetr a number: "))
+if num%2 == 0:
+    print(num," is a even number.")
+else:
+    print(num,"is a odd number.")
