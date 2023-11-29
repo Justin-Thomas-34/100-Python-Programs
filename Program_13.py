3f565688aa8e7dd35cbfb517769a9ae7e44d04ca
Justin-Thomas-34 <justinthomasus34@gmail.com>
2023-11-29 09:19:00 -0500

    program_13

---
+++ Program_13.py
@@ -0,0 +1,16 @@
+# Program 13: Python Program to Find the Largest Among Three Numbers
+
+
+# Solution
+num1 = int(input("Enter First Number: "))
+num2 = int(input("Enter Second Number: "))
+num3 = int(input("Enter Third Number: "))
+
+if (num1 > num2) and (num1 > num3):
+    print(num1," is the largest number")
+elif (num2 > num1) and (num2 > num3):
+    print(num2," is the largest number")
+else:
+    print(num3," is the largest number")
+
+
