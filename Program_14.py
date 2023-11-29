1c2342a96676e100adf4f09b006802cf249f98e8
Justin-Thomas-34 <justinthomasus34@gmail.com>
2023-11-29 09:19:12 -0500

    Program_14

---
+++ Program_14.py
@@ -0,0 +1,14 @@
+# Program 14: Python Program to Check Prime Number
+
+# Solution
+num = int(input("Enter a number: "))
+if num == 1:
+    print(num,"is not a prime number")
+if num > 1:
+    for i in range(2,num):
+        if num%i == 0:
+            print(num,"is not a prime number")
+            break
+    else:
+        print(num,"is a prime number")
+        
