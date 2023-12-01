1cea36cc2dffbf07f28b63f63689f5e2c5982102
Justin-Thomas-34 <justinthomasus34@gmail.com>
2023-12-01 11:26:07 -0500

    Program_20

---
+++ Program_20.py
@@ -0,0 +1,23 @@
+# Program 20: Python Program to Check Armstrong Number in an Interval
+
+# Armstrong Number 153 = 1*1*1 + 5*5*5 + 3*3*3 = 1 + 125 + 27 = 153 (the number in any given base , which forms the total of the same number  when each of its digits is raised to the power of the number of digits in the number)
+# 1634 = 1*1*1*1 + 6*6*6*6 + 3*3*3*3 + 4*4*4*4 = 1 + 1296 + 81 + 256 = 1634
+
+# Solution
+
+lower = int(input("Enter a number for lower limit of range: "))
+upper = int(input("Enter a number for upper limit of range: "))
+
+
+for num in range(lower,upper+1):
+    order = len(str(num))
+    sum = 0
+    temp = num
+
+    while temp > 0:
+        digit = temp%10
+        sum += digit**order
+        temp //= 10
+    
+    if sum == num:
+        print(num)
