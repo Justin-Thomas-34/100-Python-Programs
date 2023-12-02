fbb1da7fa30537906fc7d9753835f758b40c2a96
Justin-Thomas-34 <justinthomasus34@gmail.com>
2023-12-02 12:39:25 -0500

    Program_24

---
+++ Program_24.py
@@ -0,0 +1,12 @@
+# Program 24 : Python Program to Convert Decimal to Binary, Octal and Hexadecimal
+
+# Solution
+
+decimal = int(input("Enter a number here: "))
+
+print("The conversion of decimal number", decimal, "is: ")
+print(bin(decimal), " in binary")
+print(oct(decimal), " in octal")
+print(hex(decimal), " in hexadecimal")
+
+
