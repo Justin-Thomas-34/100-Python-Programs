f2cbc402801b7870d90619698fa6350732dc81da
Justin-Thomas-34 <justinthomasus34@gmail.com>
2023-12-05 10:00:59 -0500

    Program_29

---
+++ Program_29.py
@@ -0,0 +1,14 @@
+# Program 29: Python program to Shuffle Deck of Cards
+
+# Solution: Using random module and itertools
+
+import random, itertools
+
+deck = list(itertools.product(range(1,14),["Spade","Club","Heart","Diamond"]))
+random.shuffle(deck)
+
+
+print(deck)
+
+for i in range(5):
+    print(deck[i][0],"of",deck[i][1])
