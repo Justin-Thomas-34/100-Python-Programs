133a661ae69000867cc6cc7cdcd118963da61363
Justin-Thomas-34 <justinthomasus34@gmail.com>
2023-12-12 09:02:11 -0500

    Program_39

---
+++ Program_39.py
@@ -0,0 +1,13 @@
+# Program 39: Python Program to Remove Punctuations From a string.
+
+# Solution: 
+
+punc = """!()-[]{};:'"\,<>./?@#$%^&*_~`"""
+string = input("Enter anything here: ")
+empty_str = ""
+
+for i in string:
+    if i not in punc:
+        empty_str += i
+    
+print(empty_str)
