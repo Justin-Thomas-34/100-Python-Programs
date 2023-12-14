dd85a7d2efc773dc7a1dfa9514d59218243b47a4
Justin-Thomas-34 <justinthomasus34@gmail.com>
2023-12-13 21:11:37 -0500

    Program_41

---
+++ Program_41.py
@@ -0,0 +1,12 @@
+# Program 41: Python Program to Illustrate Different Set Operations
+
+# Solution:
+
+A = {1,2,3,6,8,9}
+B = {3,4,5,6,7,8}
+
+print("The Union is: ", A | B)
+print("The Intersection is:", A & B)
+print("The Difference is:", A - B)
+print("The Symmetric Difference is:", A ^ B)
+
