bde62bb881015baeb75567b1fa9ac804e9d7101f
Justin-Thomas-34 <justinthomasus34@gmail.com>
2023-12-18 20:05:20 -0500

    Program_47

---
+++ Program_47.py
@@ -0,0 +1,12 @@
+# Program 47: Python Program to Slice Lists
+
+# Solution: 
+
+a=["Joe","Rachel","Monica","Phoebe","Ross","Chandler"]
+
+print(a[1:4])
+print(a[::])
+print(a[:3])
+print(a[2:])
+print(a[::2])
+print(a[::-1])
